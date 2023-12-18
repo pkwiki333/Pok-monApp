4ecd5954047f6729704e433bc9eedb0a56e96987
Quinten Van Wiele <quinten.vanwiele@student.hogent.be>
2023-12-18 18:54:15 +0100

    UI Pokédex (Mock data)

--- app/src/main/java/com/example/androidpokemonapp/MainActivity.kt
+++ app/src/main/java/com/example/androidpokemonapp/MainActivity.kt
@@ -21,15 +21,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +40,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PokemonScreen()
+                    //PokemonScreen()
+                    PokedexScreen()
                 }
             }
         }
@@ -76,7 +76,9 @@ fun PokemonContent(padding: PaddingValues) {
             Button(
                 onClick = { /* TODO: Handler for Pokémon of the day */ },
                 colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-                modifier = Modifier.fillMaxWidth().align(Alignment.Start)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start)
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.pokeball_pokemon_svgrepo_com),
@@ -149,11 +151,146 @@ fun PokemonScreen() {
         )
     }
 }
+
 //--------------------------------------------------------------------------------------------------------------------------------
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PokemonOfTheDayScreen() {
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    Text(text ="Pokémon of the day", fontWeight = FontWeight.Bold)
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* handle back navigation */ }) {
+                        Icon(Icons.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        },
+        content = { padding ->
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(16.dp),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text("Naam: __________", style = MaterialTheme.typography.titleLarge)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    // Placeholder for the Pokémon image
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .background(Color.LightGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Foto")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
 
+                    Text("Types: __________")
+                    Text("Pokédex index: __________")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text("Moves")
+                    Text("________________")
+                    Text("________________")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text("Beschrijving")
+                    Text("________________")
+                    Text("________________")
+                }
+            }
+        }
+    )
+}
+//--------------------------------------------------------------------------------------------------------------------------------
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PokedexScreen() {
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = { Text("Pokédex") },
+                navigationIcon = {
+                    IconButton(onClick = { /* handle back navigation */ }) {
+                        Icon(Icons.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        },
+        content = { padding ->
+            LazyColumn(
+                modifier = Modifier
+                    .padding(padding)
+                    .fillMaxSize()
+            ) {
+                items(PokemonData.sampleData) { pokemon ->
+                    PokemonCard(pokemon = pokemon)
 
+                }
+            }
+        }
+    )
+}
 
+@Composable
+fun PokemonCard(pokemon: PokemonDataDC) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.backgroundtest),
+                contentDescription = "Pokémon Image",
+                modifier = Modifier.size(100.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+            ) {
+                Text(pokemon.name, style = MaterialTheme.typography.titleMedium)
+                Text("Types: ${pokemon.types}")
+                Text("Pokédex Index: ${pokemon.pokedexIndex}")
+            }
+            IconButton(onClick = { /* handle info */ }) {
+                Icon(Icons.Filled.Info, "Info")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(painter = painterResource(id = R.drawable.pokeball_pokemon_svgrepo_com), contentDescription = "pokebal", modifier = Modifier.size(24.dp))
+            }
+        }
+    }
+}
 
+data class PokemonDataDC(val name: String, val types: String, val pokedexIndex: Int)
+
+object PokemonData {
+    val sampleData = listOf(
+        PokemonDataDC("Bulbasaur", "Grass, Poison", 1),
+        PokemonDataDC("Charmander", "Fire", 2),
+        PokemonDataDC("Pikachu", "Electric", 3),
+        PokemonDataDC("Charizard", "Fire", 4),
+        PokemonDataDC("Raichu", "Fire", 5),
+        PokemonDataDC("Wurmple", "Fire", 6),
+        PokemonDataDC("Golem", "Fire", 7),
+        // Add more sample data here
+    )
+}
 
 //--------------------------------------------------------------------------------------------------------------------------------
 @Preview(showBackground = true)
